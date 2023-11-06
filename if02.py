503186892bb76984bc34d3341a6c406d717ba2a6
elmurod <elmurodraimov764@gmail.com>
2023-11-06 15:27:14 +0500

    kichik qiymat

--- if02.py
+++ if02.py
@@ -1,11 +1,9 @@
 def main(a,b,c):
-    """
-    Find the smallest of the numbers.
-    Args:
-        a: First number.
-        b: Second number.
-        c: Third number.
-    Returns:
-        int: return answer.
-    """
-    return
+    if a<b:
+        return a
+    if b<c:
+        return b
+    if c<a:
+        return c
+print(main(1,4,2))
+print(main(-5,-3,-1))
