a37a52db410f135cdb706c7419dc7aa6d9f3653d
elmurod <elmurodraimov764@gmail.com>
2023-11-07 19:35:27 +0500

    hafta kunlari

--- if08.py
+++ if08.py
@@ -1,17 +1,18 @@
 def main(number):
-    """
-    Return the days of the week, return the days of the week according to the numbers 1 to 7.
-    Use the elif statments.
-    1: "Monday"
-    2: "Tuesday"
-    3: "Wednesday"
-    4: "Thursday"
-    5: "Friday"
-    6: "Saturday"
-    7: "Sunday"
-    Args:
-        number: Number of the day.
-    Returns:
-        str: return answer.
-    """
-    return
+   
+    if number < 0:
+        return "Monday"
+    if number >= 1 and number <=2:
+        return "Tuesday"
+    if number >= 2 and number <=3:
+        return "Wednesday"
+    if number >= 3 and number <=4:
+        return "Thursday"
+    if number >= 4 and number <=5:
+        return "Friday"
+    if number >= 5 and number <=6:
+        return "Saturday"
+    if number >= 6 and number <=7:
+        return "Sunday"
+print(main(2))
+print(main(6))
