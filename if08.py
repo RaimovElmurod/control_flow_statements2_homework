ffa3de3a33831e1b7f73ab582fb7ff119fc78710
elmurod <elmurodraimov764@gmail.com>
2023-11-07 19:39:33 +0500

    hafta kunlari

--- if08.py
+++ if08.py
@@ -2,17 +2,17 @@ def main(number):
    
     if number < 0:
         return "Monday"
-    if number >= 1 and number <=2:
+    elif number >= 1 and number <=2:
         return "Tuesday"
-    if number >= 2 and number <=3:
+    elif number >= 2 and number <=3:
         return "Wednesday"
-    if number >= 3 and number <=4:
+    elif number >= 3 and number <=4:
         return "Thursday"
-    if number >= 4 and number <=5:
+    elif number >= 4 and number <=5:
         return "Friday"
-    if number >= 5 and number <=6:
+    elif number >= 5 and number <=6:
         return "Saturday"
-    if number >= 6 and number <=7:
+    elif number >= 6 and number <=7:
         return "Sunday"
 print(main(2))
 print(main(6))
