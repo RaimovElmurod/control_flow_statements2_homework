1ca8059a01d5f976cf90f61d3a1ac6374e154c8d
elmurod <elmurodraimov764@gmail.com>
2023-11-08 19:40:28 +0500

    urtadagi son

--- if03.py
+++ if03.py
@@ -1,11 +1,14 @@
 def main(a,b,c):
-    """
-    Determine the number between large and small.
-    Args:
-        a: First number.
-        b: Second number.
-        c: Third number.
-    Returns:
-        int: return answer.
-    """
-    return
+    mx = a
+    mn = a
+    if mx<b:
+        mx = b
+    if mx<c:
+        mx = c
+    if mn > b:
+        mn = b
+    if mn > c:
+        mn = c
+    return (a+b+c)-(mx+mn)
+print(main(3,7,1))
+print(main(5,5,-1))
