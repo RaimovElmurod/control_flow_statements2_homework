9d7b9ff0e94837833eb1ad66e3450373ba83aba2
elmurod <elmurodraimov764@gmail.com>
2023-11-08 10:16:03 +0500

    ohirgi qiymat

--- if05.py
+++ if05.py
@@ -1,9 +1,21 @@
 def main(n):
-    """
-    Find the largest digit of the five-digit number.
-    Args:
-        n: Five-digit number.
-    Returns:
-        int: return answer.
-    """
-    return
+    x1=n%10
+    n//=10
+    x2=n%10
+    n//=10
+    x3=n%10
+    n//=10
+    x4=n%10
+    n//=10
+    x5=n
+    a=x1
+    if x2>a:
+        a=x2
+    if x3>a:
+        a=x3
+    if x4>a:
+        a=x4
+    if x5>a:
+        a=x5
+    return a
+print(main(23546))
